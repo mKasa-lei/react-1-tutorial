e4da88cec4cb00c44ae97169149d565c5eaf6dfa
minamiriku <r.minami@leihauoli.com>
2020-10-07 23:16:35 +0900

    typescriptにする

--- react-typescript/src/index.tsx
+++ react-typescript/src/index.tsx
@@ -1,13 +1,137 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./style.scss";
 import * as serviceWorker from "./serviceWorker";
 
-const App = () => {
-  return <div>Hello, world!</div>;
+type SquareType = {
+  value: "X" | "O" | null;
+  onClick: () => void;
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Square: React.FC<SquareType> = (props) => {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+};
+
+type BoardType = {
+  squares: Array<"X" | "O" | null>;
+  onClick: (i: number) => void;
+};
+
+const Board: React.FC<BoardType> = (props) => {
+  const squareNumber = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+
+  return (
+    <div>
+      {squareNumber.map((row, index) => (
+        <div key={index} className="board-row">
+          {row.map((i) => (
+            <Square
+              key={i}
+              value={props.squares[i]}
+              onClick={() => props.onClick(i)}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const calculateWinner = (squares: Array<"X" | "O" | null>) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
+const Game: React.FC = () => {
+  const [history, setHistory] = useState([
+    {
+      squares: Array(9).fill(null),
+    },
+  ]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const historyCurrent = history;
+  const current = historyCurrent[stepNumber];
+  const winner = calculateWinner(current.squares);
+
+  const handleClick = (i: number) => {
+    const historySlice = history.slice(0, stepNumber + 1);
+    const current = historySlice[historySlice.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory(
+      historySlice.concat([
+        {
+          squares: squares,
+        },
+      ])
+    );
+    setStepNumber(historySlice.length);
+    setXIsNext(!xIsNext);
+  };
+
+  const jumpTo = (step: number) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
+
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+  const mark = xIsNext ? "X" : "O";
+  const status = winner ? "Winner: " + winner : "Next player: " + mark;
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          squares={current.squares}
+          onClick={(i: number) => handleClick(i)}
+        />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+};
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
