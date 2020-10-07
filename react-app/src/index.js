b459a1477d5e9aed0ed8352adafce24c7d86d140
k-wtnb <ko.watanabe@leihauoli.com>
2020-10-07 16:31:59 +0900

    renderSquare関数コンポーネント削除

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -103,34 +103,24 @@ const Game = () => {
 /* 盤面 */
 const Board = (props) => {
 
-  /* i番目のマス呼び出し */
-  const renderSquare = (i) => {
-    return (
-      /* function Square呼び出し */
-      /* i番目のマスをvalueとしてSquareを持たせる */
-      /* SquareコンポーネントでonClickが押されたらGameコンポーネント内のBoardのOnClickを呼び出す */
-      <Square value={props.squares[i]} onClick={() => props.onClick(i)} />
-    );
-  };
-
   /* 盤面表示 */
   return (
     /* renderSquare(i)を呼び出す */
     <div>
       <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
+      <Square value={props.squares[0]} onClick={() => props.onClick(0)} />
+      <Square value={props.squares[1]} onClick={() => props.onClick(1)} />
+      <Square value={props.squares[2]} onClick={() => props.onClick(2)} />
       </div>
       <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
+      <Square value={props.squares[3]} onClick={() => props.onClick(3)} />
+      <Square value={props.squares[4]} onClick={() => props.onClick(4)} />      
+      <Square value={props.squares[5]} onClick={() => props.onClick(5)} />
       </div>
       <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
+      <Square value={props.squares[6]} onClick={() => props.onClick(6)} />
+      <Square value={props.squares[7]} onClick={() => props.onClick(7)} />
+      <Square value={props.squares[8]} onClick={() => props.onClick(8)} />
       </div>
     </div>
   );
