07b9ed4c632a11072950132dcf61568f8cf920de
minamiriku <r.minami@leihauoli.com>
2020-10-07 10:25:01 +0900

    関数コンポーネント化

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,132 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./style.scss";
 import * as serviceWorker from "./serviceWorker";
 
-function Square(props) {
+const Square = (props) => {
   return (
     <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
-}
+};
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
+const renderSquare = (i, props) => {
+  return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
+};
 
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+const Board = (props) => {
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0, props)}
+        {renderSquare(1, props)}
+        {renderSquare(2, props)}
       </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      history: [
-        {
-          squares: Array(9).fill(null),
-        },
-      ],
-      stepNumber: 0,
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      history: history.concat([
-        {
-          squares: squares,
-        },
-      ]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: step % 2 === 0,
-    });
-  }
-
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-
-    const moves = history.map((step, move) => {
-      const desc = move ? "Go to move #" + move : "Go to game start";
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-    }
-
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
+      <div className="board-row">
+        {renderSquare(3, props)}
+        {renderSquare(4, props)}
+        {renderSquare(5, props)}
       </div>
-    );
-  }
-}
+      <div className="board-row">
+        {renderSquare(6, props)}
+        {renderSquare(7, props)}
+        {renderSquare(8, props)}
+      </div>
+    </div>
+  );
+};
 
-// ========================================
-
-ReactDOM.render(<Game />, document.getElementById("root"));
-
-function calculateWinner(squares) {
+const calculateWinner = (squares) => {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -144,7 +55,95 @@ function calculateWinner(squares) {
     }
   }
   return null;
-}
+};
+
+const handleClick = (
+  i,
+  history,
+  stepNumber,
+  xIsNext,
+  setHistory,
+  setStepNumber,
+  setXIsNext
+) => {
+  const historySlice = history.slice(0, stepNumber + 1);
+  const current = historySlice[historySlice.length - 1];
+  const squares = current.squares.slice();
+  if (calculateWinner(squares) || squares[i]) {
+    return;
+  }
+  squares[i] = xIsNext ? "X" : "O";
+  setHistory(
+    history.concat([
+      {
+        squares: squares,
+      },
+    ])
+  );
+  setStepNumber(historySlice.length);
+  setXIsNext(!xIsNext);
+};
+
+const jumpTo = (step, setStepNumber, setXIsNext) => {
+  setStepNumber(step);
+  setXIsNext(step % 2 === 0);
+};
+
+const Game = () => {
+  const [history, setHistory] = useState([
+    {
+      squares: Array(9).fill(null),
+    },
+  ]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const historySlice = history;
+  const current = historySlice[stepNumber];
+  const winner = calculateWinner(current.squares);
+
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move, setStepNumber, setXIsNext)}>
+          {desc}
+        </button>
+      </li>
+    );
+  });
+  const mark = xIsNext ? "X" : "O";
+  const status = winner ? "Winner: " + winner : "Next player: " + mark;
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          squares={current.squares}
+          onClick={(i) =>
+            handleClick(
+              i,
+              history,
+              stepNumber,
+              xIsNext,
+              setHistory,
+              setStepNumber,
+              setXIsNext
+            )
+          }
+        />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+};
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
