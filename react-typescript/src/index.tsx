a94d8608ad0228194856e064349fe724e5c19c03
k-wtnb <ko.watanabe@leihauoli.com>
2020-10-09 18:52:26 +0900

    オプション機能追加

--- react-typescript/src/index.tsx
+++ react-typescript/src/index.tsx
@@ -6,30 +6,39 @@ import "./style.scss";
 const Game: React.FC = () => {
   /* クラスにコンストラクタを追加し、stateを初期化 */
   /* 9マス全ての状態をnullに設定 */
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState<Array<string>>(Array(9).fill(null));
   /* 初手の盤面の状態 */
-  const [history, setHistory] = useState([{ squares }]);
+  const [history, setHistory] = useState<Array<{ squares: Array<string> }>>([
+    { squares },
+  ]);
   /*0手目の状態を見ている */
-  const [stepNumber, setStepNumber] = useState(0);
+  const [stepNumber, setStepNumber] = useState<number>(0);
   /*Xの手番*/
-  const [xIsNext, setXIsNext] = useState(true);
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+
+  const [click, setClick] = useState<boolean>(false);
 
   /* i番目のマスを押した時の挙動 */
-  const handleClick = (i: number) => {
+  const handleClick: Function = (i: number) => {
+
+    setClick(true);
     /* イミュータ（望む変更を加えた新しいデータのコピーで古いデータを置き換える） */
     /* squaresのコピー作成*/
     /* ０手目から現在までの履歴をコピー*/
-    const untilHistory = history.slice(0, stepNumber + 1); //slice(start,end) endは含まれない
+    const untilHistory: Array<{ squares: Array<string> }> = history.slice(
+      0,
+      stepNumber + 1
+    ); //slice(start,end) endは含まれない
     /* 現在の状態 */
-    const current = history[untilHistory.length - 1];
+    const current: { squares: Array<string> } =
+      history[untilHistory.length - 1];
 
     /* 現在の盤面のコピー作成*/
-    const squares = current.squares.slice();
+    const squares: Array<string> = current.squares.slice();
     /* ゲームが終わっているorそのマスがすでに埋まっているならreturn */
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
-
     /* this.state.xIsNextがtrueならば'X'、falseならば'O'をsquares[i]に上書き */
     squares[i] = xIsNext ? "X" : "O";
     /* 現在の盤面 */
@@ -40,10 +49,11 @@ const Game: React.FC = () => {
     setStepNumber(untilHistory.length);
     /* Xの手番がtrueならばfalseに,falseならばtrue */
     setXIsNext(!xIsNext);
+ 
   };
 
   /* move番目の履歴ボタンを押した時の挙動 */
-  const jumpTo = (step: number) => {
+  const jumpTo: Function = (step: number) => {
     /* step(move)手目の状態をみている */
     setStepNumber(step);
     /* stepNumerの値が偶数ならばxIsNextをtrue */
@@ -51,19 +61,19 @@ const Game: React.FC = () => {
   };
 
   /* 初手から現在までの盤面の状態 */
-  const currentHistory = history;
+  const currentHistory: Array<{ squares: Array<string> }> = history;
   /* 現在の盤面の状態 */
-  const current = currentHistory[stepNumber];
+  const current: { squares: Array<string> } = currentHistory[stepNumber];
   /* 現在のゲーム状態勝敗確認 */
-  const winner = calculateWinner(current.squares); /* 履歴を並べる */
-  const moves = currentHistory.map((step, move) => {
-    const desc = move ? "Go to move #" + move : "Go to game start";
+  const winner: Function = calculateWinner(current.squares); /* 履歴を並べる */
+  const moves: object = currentHistory.map((step, move) => {
+    const desc: string = move ? "Go to move #" + move : "Go to game start";
 
     /* ゲーム履歴ボタンを表示 */
     return (
       <li key={move}>
         {/* ボタンがクリックされたら、jumptoメソッドをmoveの値を持って呼び出す */}
-        <button onClick={() => jumpTo(move)}>{desc}</button>
+        <button className="history-btn" onClick={() => jumpTo(move)}>{desc}</button>
       </li>
     );
   });
@@ -84,19 +94,20 @@ const Game: React.FC = () => {
   return (
     <div className="game">
       <div className="game-board">
+         {/* status状態を表示 */}
+         <div className="status">{status}</div>
         {/* Boardコンポーネントを呼び出し */}
         {/* current,squaresをsquaresとして持たせる */}
         {/* BoardコンポーネントでonClickが押されたらhandleClickを呼び出し */}
         <Board
           squares={current.squares}
           onClick={(i: number) => handleClick(i)}
+          click={click}
         />
       </div>
       <div className="game-info">
-        {/* status状態を表示 */}
-        <div>{status}</div>
         {/* movesを表示 */}
-        <ol>{moves}</ol>
+        <ol className="history-list">{moves}</ol>
       </div>
     </div>
   );
@@ -105,22 +116,24 @@ const Game: React.FC = () => {
 type PropsGame = {
   squares: Array<string>;
   onClick: Function;
+  click:boolean;
 };
 
 /* 盤面 */
 const Board: React.FC<PropsGame> = (props) => {
-  const squares = [
+  const squares: Array<Array<number>> = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
   ];
-  const squareList = squares.map((line, column) => {
-    const square = line.map((lineValue, lineOrder) => {
+  const squareList: object = squares.map((line, column) => {
+    const square: object = line.map((lineValue, lineOrder) => {
       return (
         <Square
           key={lineOrder}
           value={props.squares[lineValue]}
           onClick={() => props.onClick([lineValue])}
+          click={props.click}
         />
       );
     });
@@ -133,12 +146,13 @@ const Board: React.FC<PropsGame> = (props) => {
     );
   });
 
-  return <div>{squareList}</div>;
+  return <div className="game-board-inner">{squareList}</div>;
 };
 
 type PropsBoard = {
   value: String;
   onClick: () => void /* Function */;
+  click:boolean;
 };
 
 /* 1マスの表示 */
@@ -146,7 +160,7 @@ type PropsBoard = {
 const Square: React.FC<PropsBoard> = (props) => {
   return (
     /* マスが押されたらBoardのSquareのonClickを呼び出す */
-    <button className="square" onClick={props.onClick}>
+    <button className={props.click===true?(props.value==="X"?"square x click":(props.value==="O"?"square o click":"square")):"square"} onClick={props.onClick}>
       {/* props.value(suare)を○×を表示 */}
       {props.value}
     </button>
@@ -155,9 +169,9 @@ const Square: React.FC<PropsBoard> = (props) => {
 
 /* 勝敗判定 */
 /* 盤面状態を引数に持つ */
-const calculateWinner = (squares: Array<string> = []) => {
+const calculateWinner: Function = (squares: Array<string> = []) => {
   /* 縦横斜めのline */
-  const lines = [
+  const lines: Array<Array<number>> = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -170,7 +184,7 @@ const calculateWinner = (squares: Array<string> = []) => {
   /* line配列分forを実行 */
   for (let i = 0; i < lines.length; i++) {
     /* i番目のlineを[a,b,c]に代入 */
-    const [a, b, c] = lines[i];
+    const [a, b, c]: Array<number> = lines[i];
     /* a,b,cが全て同じであれば */
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       /* aの中身を返す（○ or ×） */
